5ba51d9e8ddb597540aed61d71d5060f44e508eb
Jiří Pudil <me@jiripudil.cz>
2024-12-01 09:47:59 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,38 @@
+import kotlin.math.abs
+
 fun main() {
+    fun buildLists(input: List<String>): Pair<List<Int>, List<Int>> {
+        val a = mutableListOf<Int>()
+        val b = mutableListOf<Int>()
+
+        input.forEach { line ->
+            val (a_no, b_no) = line.split(Regex("\\s+"))
+            a.add(a_no.toInt())
+            b.add(b_no.toInt())
+        }
+
+        return Pair(a, b)
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val (aUnsorted, bUnsorted) = buildLists(input)
+        val a = aUnsorted.sorted()
+        val b = bUnsorted.sorted()
+
+        return a.mapIndexed { index, _ -> abs(a[index] - b[index]) }.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (a, b) = buildLists(input)
+
+        val bCounts = b.groupBy { it }.mapValues { it.value.size }
+        return a.fold(0) { acc, i -> acc + i * bCounts.getOrElse(i, { 0 }) }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
